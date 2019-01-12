9bb765778e6cdb6e57870cbf6a7db1cf6e95640e
Paul Amazona <whatevergeek@gmail.com>
2019-01-12 13:59:22 +0800

    Add project files.

---
+++ sgdemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sgdemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
